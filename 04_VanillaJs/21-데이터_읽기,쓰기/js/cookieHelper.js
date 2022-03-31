0bf515a82eb284a480473c37a98fb273c67a9885
chanCo <loinsective@naver.com>
2022-03-31 20:15:28 +0900

    연습문제

--- 04_VanillaJs/21-데이터_읽기,쓰기/js/cookieHelper.js
+++ 04_VanillaJs/21-데이터_읽기,쓰기/js/cookieHelper.js
@@ -8,10 +8,14 @@ class CookieHelper {
    * @param {json} options - 유효시간, 유효경로, 도메인 등을 json으로 묶어서 보낸다.
    */
   setCookie(name, value, options={}) {
-    if(options.path == undefined) { options.path = "/"; };
+    if(options.path == undefined) { 
+      options.path = "/"; 
+    };
 
     // expires 값이 Date 클래스의 객체라면 UTCString 타입으로 변환함. (max-age가 있으면 사용 안해도 된다.) -> 2021-11-25T05:09:30.569Z
-    if(options.expires instanceof Date) { options.expires = options.expires.toUTCString(); };
+    if(options.expires instanceof Date) { 
+      options.expires = options.expires.toUTCString(); 
+    };
 
     // "이름=값" 형식으로 저장할 문자열을 만듦
     let updateCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
