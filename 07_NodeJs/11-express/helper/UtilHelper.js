efda3b52484eeafe179d537c8e4d16c8be94dc45
chanCo <loinsective@naver.com>
2022-06-29 14:27:02 +0900

    node 5일차

---
+++ 07_NodeJs/11-express/helper/UtilHelper.js
@@ -0,0 +1,28 @@
+/**
+ * @Filename: UtilHelper.js
+ * @Description: 백엔드 개발시 자주 사용되는 독립 함수들 모음
+ * @Author:
+ */
+
+import { networkInterfaces } from 'os';
+
+const myIp = () => {
+  const ipAddress = [];
+  const nets = networkInterfaces();
+
+  for(const attr in nets) {
+    const item = nets[attr];
+    
+    item.map((v,i) => {
+      if (v.family == 'IPv4' && v.address != '127.0.0.1') {
+      // if (v.family == 4 && v.address != '127.0.0.1') {
+        ipAddress.push(v.address);
+      }
+    });
+  }
+  return ipAddress;
+};
+
+const urlFormat = (urlObject) => String(Object.assign(new URL('http://a.com'), urlObject));
+
+export { myIp, urlFormat };
