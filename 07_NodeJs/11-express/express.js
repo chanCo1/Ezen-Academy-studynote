de4168c0ca5ca2bd33bdf9f54fcdd5091b5ece99
chanCo <loinsective@naver.com>
2022-07-01 15:25:07 +0900

    node 7일차

--- 07_NodeJs/11-express/express.js
+++ 07_NodeJs/11-express/express.js
@@ -19,6 +19,7 @@ import bodyParser from 'body-parser';           // POST 파라미터 처리
 import methodOverride from 'method-override';   // PUT 파라미터 처리
 import cookieParser from 'cookie-parser';       // Cookie 처리
 import expressSession from 'express-session'    // Session 처리
+import nodemailer from 'nodemailer';            // 메일발송 -> app.use()로 추가 설정 없음
 
 
 /* - - - - - - - - - - - - - - - - - - - -
@@ -446,6 +447,65 @@ router
     res.status(result_code).send(json);
   });
 
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 06-SendMail.js
+// public/06_mail.html
+router.post('/send_mail', async (req, res, next) => {
+  /** 1) 프론트엔드에서 전달한 사용자 입력값 */
+  const writer_name = req.body.writer_name;
+  let writer_email = req.body.writer_email;
+  const receiver_name = req.body.receiver_name;
+  let receiver_email = req.body.receiver_email;
+  const subject = req.body.subject;
+  const content = req.body.content;
+
+  /** 2) 보내는 사람, 받는 사람의 메일 주소와 이름 */
+  // 보내는 사람의 이름과 주소
+  // -> 외부 SMTP 연동시 주의사항 :: 발신주소가 로그인 계정과 다를 경우 발송이 거부된다.
+  if(writer_name) {
+    // ex) 홍길동 <gildong1234@mail.com>
+    writer_email = writer_name + ' <' + writer_email + '>';
+  }
+
+  // 받는 사람의 이름과 주소
+  if(receiver_name) {
+    receiver_email = receiver_name + ' <' + receiver_email + '>';
+  }
+
+  /** 3) 메일 발송정보 구성 */
+  const send_info = {
+    from: writer_email,
+    to: receiver_email,
+    subject: subject,
+    html: content,
+  };
+
+  /** 4) 발송에 필요한 서버 정보를 사용하여 발송객체 생성 */
+  const smtp = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,        // SMTP 서버명 : smtp.gmail.com
+    port: process.env.SMTP_PORT,        // SMTP 포트 : 465
+    secure: true,                       // 보안연결(SSL) 필요
+    auth: {
+      user: process.env.SMTP_USERNAME,  // Gamil 로그인에 사용하는 메일주소
+      pass: process.env.SMTP_PASSWORD,  // 앱 비밀번호
+    }
+  });
+
+  /** 5) 메일발송 요청 */
+  let rt = 200;
+  let rtMsg = 'success';
+
+  try {
+    await smtp.sendMail(send_info);
+  } catch (e) {
+    rt = 500;
+    rtMsg = e.message;
+  }
+
+  res.status(rt).send(rtMsg);
+});
+
+
 /* - - - - - - - - - - - - - - - - - - - -
 6) 설정한 내용을 기반으로 서버 구동 시작
 - - - - - - - - - - - - - - - - - - - - */
@@ -462,3 +522,11 @@ app.listen(process.env.PORT, () => {
 
   logger.debug('- - - - - - - - - - - - - - - - - -')
 });
+
+
+
+
+
+// 구글 연동 비번 (띄어쓰기 없음!)
+// nzidxjhrrunvtdjg
+// xbeaayddaxhuypyq
--- 07_NodeJs/11-express/test.js
+++ 07_NodeJs/11-express/test.js
@@ -1,10 +1,10 @@
 import express from 'express';               // express 호출
 import bodyParser from 'body-parser';        // POST 파라미터 처리
-import expressSession from 'express-session' // Session 처리
 import serveStatic from 'serve-static';      // 특정 폴더의 파일을 URL로 노출시킴
+import nodemailer from 'nodemailer';         // 메일발송 -> app.use()로 추가 설정 없음
 
 const app = express();  // express 사용
-const port = 3001;      // 포트 번호 지정
+const port = 3002;      // 포트 번호 지정
 
 /** HTML, CSS, IMG, JS 등의 정적 파일을 URL에 노출시킬 폴더 연결 */
 // "http://아이피(혹은 도메인):포트번호" 이후의 경로가 router에 등록되지 않은 경로라면 static 모듈에 연결된 폴더 안에서 해당 경로를 탐색한다.
@@ -18,83 +18,67 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());  // TEXT형식의 파라미터 수신 가능
 app.use(bodyParser.json());  // JSON형식의 파라미터 수신 가능
 
-/** 세션 설정 */
-app.use(expressSession({
-  // 암호화 키
-  secret: 'test123',
-  // 세션이 초기화 되지 않더라도 새로 저장할지 여부 (일반적으로 false)
-  resave: false,
-  // 세션이 저장되기 전에 기존의 세션을 초기화 상태로 만들지 여부
-  saveUninitialized: false,
-}));
+app.post('/send_mail', async (req, res, next) => {
+  /** 1) 프론트엔드에서 전달한 사용자 입력값 */
+  const writer_name = req.body.writer_name;
+  let writer_email = req.body.writer_email;
+  const receiver_name = req.body.receiver_name;
+  let receiver_email = req.body.receiver_email;
+  const subject = req.body.subject;
+  const content = req.body.content;
 
-app
-  .post('/session/login', (req, res, next) => {
-    const id = req.body.userid;
-    const pw = req.body.userpw;
+  /** 2) 보내는 사람, 받는 사람의 메일 주소와 이름 */
+  // 보내는 사람의 이름과 주소
+  // -> 외부 SMTP 연동시 주의사항 :: 발신주소가 로그인 계정과 다를 경우 발송이 거부된다.
+  if(writer_name) {
+    // ex) 홍길동 <gildong1234@mail.com>
+    writer_email = writer_name + ' <' + writer_email + '>';
+  }
 
-    console.log('id = ' + id);
-    console.log('pw = ' + pw);
+  // 받는 사람의 이름과 주소
+  if(receiver_name) {
+    receiver_email = receiver_name + ' <' + receiver_email + '>';
+  }
 
-    let login_ok = false;
-    // 원래는 데이터베이스에서 값이 같은지를 확인해야한다.
-    if(id == 'node' && pw == '1234') {
-      console.log('로그인 성공');
-      login_ok = true;
+  /** 3) 메일 발송정보 구성 */
+  const send_info = {
+    from: writer_email,
+    to: receiver_email,
+    subject: subject,
+    html: content,
+  };
+
+  /** 4) 발송에 필요한 서버 정보를 사용하여 발송객체 생성 */
+  const smtp = nodemailer.createTransport({
+    // host: process.env.SMTP_HOST,        // SMTP 서버명 : smtp.gmail.com
+    // port: process.env.SMTP_PORT,        // SMTP 포트 : 465
+    // secure: true,                       // 보안연결(SSL) 필요
+    // auth: {
+    //   user: process.env.SMTP_USERNAME,  // Gamil 로그인에 사용하는 메일주소
+    //   pass: process.env.SMTP_PASSWORD,  // 앱 비밀번호
+    // }
+    host: 'smtp.gmail.com',        // SMTP 서버명 : smtp.gmail.com
+    port: '465',        // SMTP 포트 : 465
+    secure: true,                       // 보안연결(SSL) 필요
+    auth: {
+      user: 'loinsective@gmail.com',  // Gamil 로그인에 사용하는 메일주소
+      pass: 'xbeaayddaxhuypyq',  // 앱 비밀번호
     }
-
-    let result_code = null;
-    let result_msg = null;
-
-    if(login_ok) {
-      req.session.userid = id;
-      req.session.userpw = pw;
-      result_code = 200;
-      result_msg = 'success';
-    } else {
-      result_code = 403;
-      result_msg = 'fail'
-    };
-
-    const json = { rt: result_msg };
-    res.status(result_code).send(json);
-  })
-  .delete('/session/login', async (req, res, next) => {
-    let result = 'success';
-    let code = 200;
-
-    try {
-      await req.session.destroy();
-    } catch (e) {
-      logger.error(e.message);
-      result = e.message;
-      code = 500;
-    }
-
-    const json = { rt: result };
-    res.status(code).send(json);
-  })
-  .get('/session/login', (req, res, next) => {
-    const id = req.session.userid;
-    const pw = req.session.userpw;
-
-    let result_code = null;
-    let result_msg = null;
-
-    // id와 pw값이 있다면
-    if(id !== undefined && pw !== undefined) {
-      console.log('현재 로그인 상태입니다.');
-      result_code = 200;
-      result_msg = 'success';
-    } else {
-      console.log('현재 로그인 상태가 아닙니다.');
-      result_code = 400;
-      result_msg = 'fail';
-    }
-
-    const json = { rt: result_msg };
-    res.status(result_code).send(json);
   });
+
+  /** 5) 메일발송 요청 */
+  let rt = 200;
+  let rtMsg = 'success';
+
+  try {
+    await smtp.sendMail(send_info);
+  } catch (e) {
+    rt = 500;
+    rtMsg = e.message;
+  }
+
+  res.status(rt).send(rtMsg);
+});
 
 app.listen(port, () => {
   console.log('-----------------------------------');
